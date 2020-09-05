77a2df8d77c5372d4bb62321a4d1b9187e033e22
ops <dmitriycr3w@gmail.com>
2020-09-05 21:55:11 +0300

    final commit

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -127,6 +128,10 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+ABSOLUTE_URL_OVERRIDES = {
+    'auth.user': lambda u: reverse_lazy('user_detail', args=[u.username])
+}
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
